227743d38c918c2063dfb5acf5bf872f249f0a46
unknown <vishal.141414@gmail.com>
2016-09-22 20:47:43 +0530

    Bug Issue For Customer Order Email.(Name Email Array Issue Warning in Log) Desc: Note: Date:22-9-2016

--- app/code/community/Mage/Smtp/Model/Email/Template.php
+++ app/code/community/Mage/Smtp/Model/Email/Template.php
@@ -1,13 +1,12 @@
 <?php
-class Mage_Smtp_Model_Email_Template extends Mage_Core_Model_Email_Template
-{
-    public function send($email, $name=null, array $variables = array())
-    {
-		if (!Mage::getStoreConfig('smtp/settings/enabled'))
-		{
-			return parent::send($email, $name, $variables);
-		}
-        if(!$this->isValidForSend()) {
+
+class Mage_Smtp_Model_Email_Template extends Mage_Core_Model_Email_Template {
+
+    public function send($email, $name = null, array $variables = array()) {
+        if (!Mage::getStoreConfig('smtp/settings/enabled')) {
+            return parent::send($email, $name, $variables);
+        }
+        if (!$this->isValidForSend()) {
             return false;
         }
 
@@ -21,7 +20,12 @@ class Mage_Smtp_Model_Email_Template extends Mage_Core_Model_Email_Template
         $mail = $this->getMail();
         if (is_array($email)) {
             foreach ($email as $emailOne) {
-            	$mail->addTo($emailOne, $name);
+
+                if (is_array($name))
+                    $name = $name[0];
+                //var_dump($name);
+                //die;
+                $mail->addTo($emailOne, $name);
             }
         }
         else {
@@ -31,7 +35,7 @@ class Mage_Smtp_Model_Email_Template extends Mage_Core_Model_Email_Template
         $this->setUseAbsoluteLinks(true);
         $text = $this->getProcessedTemplate($variables, true);
 
-        if($this->isPlain()) {
+        if ($this->isPlain()) {
             $mail->setBodyText($text);
         } else {
             $mail->setBodyHTML($text);
@@ -39,15 +43,18 @@ class Mage_Smtp_Model_Email_Template extends Mage_Core_Model_Email_Template
 
         $mail->setSubject($this->getProcessedTemplateSubject($variables));
         $mail->setFrom($this->getSenderEmail(), $this->getSenderName());
-		
-		$transport = Mage::helper('smtp')->getTransport();
-		try {
+
+        $transport = Mage::helper('smtp')->getTransport();
+        //echo get_class($transport);
+        //echo get_class($mail);
+        //die;
+        try {
             $mail->send($transport); // Zend_Mail warning..
             $this->_mail = null;
-        }
-        catch (Exception $e) {
+        } catch (Exception $e) {
             return false;
         }
         return true;
     }
+
 }
