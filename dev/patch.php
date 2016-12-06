2b06c9c76cb27ca0ea752599d36b3eb147611997
unknown <vishal.141414@gmail.com>
2016-12-06 21:00:09 +0530

    Remove Credentials And Dev sub Domain Files. Desc: Remove Product Compare & Css Changes Note: Date:6-12-2016

--- dev/patch.php
+++
@@ -1,6 +0,0 @@
-<?php
-	print("<PRE>");
-	passthru("/bin/bash PATCH_SUPEE-6285_CE_1.9.1.1_v2.sh");
-	print("</PRE>");
-	echo "Done";
-?>
--- dev/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'app/Mage.php';
-Mage::app();
-
-echo Mage::getModel('core/date')->date('Y-m-d H:i:s');
