651c7857cb62a5371dc466a7416a8dff56aaf074
unknown <vishal.141414@gmail.com>
2017-05-14 11:46:56 +0530

    Temporary OAuth And Layer Navigation Test.

--- oauth_customer.php
+++ oauth_customer.php
@@ -1,5 +1,56 @@
 <?php
-require_once 'app/Mage.php';
-Mage::app();
+/**
+ * Example of retrieving the products list using Customer account via Magento REST API. OAuth authorization is used
+ * Preconditions:
+ * 1. Install php oauth extension
+ * 2. If you were authorized as an Admin before this step, clear browser cookies for 'yourhost'
+ * 3. Create at least one product in Magento and enable it for viewing in the frontend
+ * 4. Configure resource permissions for Customer REST user for retrieving all product data for Customer
+ * 5. Create a Consumer
+ */
+// $callbackUrl is a path to your file with OAuth authentication example for the Customer user
+$callbackUrl = "http://localhost:81/git_indiankalaniketan.com/oauth_customer.php";
+$temporaryCredentialsRequestUrl = "http://localhost:81/git_indiankalaniketan.com/oauth/initiate?oauth_callback=" . urlencode($callbackUrl);
+$customerAuthorizationUrl = 'http://localhost:81/git_indiankalaniketan.com/oauth/authorize';
+$accessTokenRequestUrl = 'http://localhost:81/git_indiankalaniketan.com/oauth/token';
+$apiUrl = 'http://localhost:81/git_indiankalaniketan.com/api/rest';
+$consumerKey = 'a7d50df0bd16ac4d2ad50f6460dde2ec';
+$consumerSecret = 'a182f272fca824bf3454b27eebe48760';
 
-echo Mage::getModel('core/date')->date('Y-m-d H:i:s');
+session_start();
+if (!isset($_GET['oauth_token']) && isset($_SESSION['state']) && $_SESSION['state'] == 1) {
+    $_SESSION['state'] = 0;
+}
+try {
+    $authType = ($_SESSION['state'] == 2) ? OAUTH_AUTH_TYPE_AUTHORIZATION : OAUTH_AUTH_TYPE_URI;
+    $oauthClient = new OAuth($consumerKey, $consumerSecret, OAUTH_SIG_METHOD_HMACSHA1, $authType);
+    $oauthClient->enableDebug();
+
+    if (!isset($_GET['oauth_token']) && !$_SESSION['state']) {
+        $requestToken = $oauthClient->getRequestToken($temporaryCredentialsRequestUrl);
+        $_SESSION['secret'] = $requestToken['oauth_token_secret'];
+        $_SESSION['state'] = 1;
+        header('Location: ' . $customerAuthorizationUrl . '?oauth_token=' . $requestToken['oauth_token']);
+        exit;
+    } else if ($_SESSION['state'] == 1) {
+        $oauthClient->setToken($_GET['oauth_token'], $_SESSION['secret']);
+        $accessToken = $oauthClient->getAccessToken($accessTokenRequestUrl);
+        $_SESSION['state'] = 2;
+        $_SESSION['token'] = $accessToken['oauth_token'];
+        $_SESSION['secret'] = $accessToken['oauth_token_secret'];
+        header('Location: ' . $callbackUrl);
+        exit;
+    } else {
+        $oauthClient->setToken($_SESSION['token'], $_SESSION['secret']);
+
+        $resourceUrl = "$apiUrl/products";
+        $oauthClient->fetch($resourceUrl, array(), 'GET', array('Content-Type' => 'application/json'));
+        $productsList = json_decode($oauthClient->getLastResponse());
+        print_r($productsList);
+    }
+} catch (OAuthException $e) {
+    print_r($e->getMessage());
+    echo "<br/>";
+    print_r($e->lastResponse);
+}
+session_destroy();
--- test.php
+++ test.php
@@ -3,3 +3,28 @@ require_once 'app/Mage.php';
 Mage::app();
 
 echo Mage::getModel('core/date')->date('Y-m-d H:i:s');
+
+$layer = Mage::getModel("catalog/layer");
+//foreach($categories as $categoryid) {
+    $category = Mage::getModel("catalog/category")->load(4);
+    $layer->setCurrentCategory($category);
+    $attributes = $layer->getFilterableAttributes();
+    
+
+    foreach ($attributes as $attribute) {
+        if ($attribute->getAttributeCode() == 'price') {
+            $filterBlockName = 'catalog/layer_filter_price';
+        } elseif ($attribute->getBackendType() == 'decimal') {
+            $filterBlockName = 'catalog/layer_filter_decimal';
+        } else {
+            $filterBlockName = 'catalog/layer_filter_attribute';
+        }
+
+        $result = Mage::app()->getLayout()->createBlock($filterBlockName)->setLayer($layer)->setAttributeModel($attribute)->init();
+        var_dump($result->getItems());
+        
+        foreach($result->getItems() as $option) {
+            echo $option->getLabel().'<br/>';
+            echo $option->getValue();
+        }
+}
